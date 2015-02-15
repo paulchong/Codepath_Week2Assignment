c95054db6718ca88d3ab830a152427098b818b6d
Paul Chong <paulchong.nz@gmail.com>
2015-02-15 11:37:23 -0800

    committing draft app

--- Week2Project_Carousel/IntroViewController.swift
+++ Week2Project_Carousel/IntroViewController.swift
@@ -1,48 +1,41 @@
 //
-//  SettingsViewController.swift
+//  IntroViewController.swift
 //  Week2Project_Carousel
 //
-//  Created by Paul Chong on 2/11/15.
+//  Created by Paul Chong on 2/15/15.
 //  Copyright (c) 2015 Codepath. All rights reserved.
 //
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-    
+class IntroViewController: UIViewController {
 
-    @IBOutlet weak var settingsScrollView: UIScrollView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
-    @IBOutlet weak var settingsImage: UIImageView!
+    @IBOutlet weak var introImage: UIImageView!
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        settingsScrollView.contentSize = settingsImage.frame.size
+
         
+        scrollView.contentSize = introImage.frame.size
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func didPressBackButton(sender: AnyObject) {
-        navigationController!.popViewControllerAnimated(true)
-    }
-    
 
-
-    
     /*
     // MARK: - Navigation
-    
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
     */
-    
+
 }
--- Week2Project_Carousel/SignInViewController.swift
+++ Week2Project_Carousel/SignInViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  SignInViewController.swift
 //  Week2Project_Carousel
@@ -9,42 +10,105 @@
 import UIKit
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     
-    
     @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var loginContainer: UIView!
+    
+    @IBOutlet weak var inputContainers: UIView!
+    
+    @IBOutlet weak var loginTextImage: UIImageView!
+
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        loadingIndicator.hidden = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     @IBAction func didPressSignInButton(sender: AnyObject) {
-        if(emailTextField.text == "email" && passwordTextField.text == "pw") {
-        performSegueWithIdentifier("loginSegue", sender:self)
-        } else {
-            var alertView = UIAlertView(title: "Oops", message: "wrong email/password", delegate: nil, cancelButtonTitle: "ok")
-        }
+        println("hey")
+        loadingIndicator.hidden = false
+        loadingIndicator.startAnimating()
+        delay(2, { () -> () in
+            if (self.emailTextField.text == "e" && self.passwordTextField.text == "pw") {
+                println("madeit")
+                self.performSegueWithIdentifier("loginSegue", sender: self)
+            } else {
+                var alertView = UIAlertView(title: "Oops", message: "email/password is incorrect", delegate: nil, cancelButtonTitle: "OK")
+                alertView.show()
+            }
+            self.loadingIndicator.stopAnimating()
+            self.loadingIndicator.hidden = true
+        })
+        
+    }
+    
+    
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        var userInfo = notification.userInfo!
+        
+        // Get the keyboard height and width from the notification
+        // Size varies depending on OS, language, orientation
+        var kbSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().size
+        var durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey] as NSNumber
+        var animationDuration = durationValue.doubleValue
+        var curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey] as NSNumber
+        var animationCurve = curveValue.integerValue
+        
+        UIView.animateWithDuration(animationDuration, delay: 0.0, options: UIViewAnimationOptions(UInt(animationCurve << 16)), animations: {
+            
+            self.loginContainer.center.y = kbSize.height
+            
+            self.loginTextImage.hidden = true
+            
+            self.inputContainers.center.y = self.loginContainer.center.y - 120
+            
+            }, completion: nil)
         
         
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        var userInfo = notification.userInfo!
+        
+        // Get the keyboard height and width from the notification
+        // Size varies depending on OS, language, orientation
+        var kbSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().size
+        var durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey] as NSNumber
+        var animationDuration = durationValue.doubleValue
+        var curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey] as NSNumber
+        var animationCurve = curveValue.integerValue
+        
+        UIView.animateWithDuration(animationDuration, delay: 0.0, options: UIViewAnimationOptions(UInt(animationCurve << 16)), animations: {
+            
+            // Set view properties in here that you want to match with the animation of the keyboard
+            // If you need it, you can use the kbSize property above to get the keyboard width and height.
+            }, completion: nil)
     }
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
